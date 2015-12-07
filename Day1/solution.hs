6d554b3c106a149de3adfdac7f094f6b2bbdf42c
gochoa1 <gochoa1@binghamton.edu>
2015-12-07 13:13:58 -0500

    Updated Haskell Comments

--- Day1/solution.hs
+++ Day1/solution.hs
@@ -9,11 +9,29 @@ convert c =
         -1
 
 basement :: [Int] -> Int -> Int -> Int
-basement [] f step = step 
+basement [] f step = -1 
 basement xs (-1) step = step 
 basement (next:xs) f step = 
     basement xs (f+next) (step+1)
     
+--( ( ( ) ) ) ( ) ) ) )
+
+--1 1 1 -1 -1 -1 1 -1 -1 -1 -1 
+-- -3
+--[1 1 1 -1 -1 -1 1 -1 -1 -1 -1 ] 0 0
+--[1 1 -1 -1 -1 1 -1 -1 -1 -1 ] 0+1 0+1
+--[1 -1 -1 -1 1 -1 -1 -1 -1 ] 2 2
+--[-1 -1 -1 1 -1 -1 -1 -1 ] 3 3
+--[-1 -1 1 -1 -1 -1 -1 ] 2 4
+--[-1 1 -1 -1 -1 -1 ] 1 5
+--[1 -1 -1 -1 -1 ] 0 6
+--[-1 -1 -1 -1 ] 1 7
+--[-1 -1 -1 ] 0 8
+--[-1 -1 ] -1 9
+
+
+
+
 main = do
     z <- getLine
     let nums = map convert z
@@ -22,3 +40,38 @@ main = do
     putStr . show $ floors
     putStr " "
     print index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
